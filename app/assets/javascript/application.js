9baa6dc13a193767dfc98d868b135b875625d9a7
Santhosh Ramaiah <Santhosh.r411@gmail.com>
2024-09-21 19:51:12 +0530

    fix issues

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,9 +1,2 @@
-// app/javascript/packs/application.js
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+// app/assets/javascripts/application.js
+//= require rails-ujs
